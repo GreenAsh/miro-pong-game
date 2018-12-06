33f462f7fde794c87a8ac52d55c6f74fde340217
Aleksey Demshin <demansh17@gmail.com>
2018-12-06 13:23:34 +0500

    global.js

--- static/web-plugin/global.js
+++ static/web-plugin/global.js
@@ -10,4 +10,29 @@ const gridInit = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
-let gameWidgets = []
+const BLOCK_SIZE = 190
+const BLOCK_STEP = 200
+const block = {
+    width: BLOCK_SIZE,
+    height: BLOCK_SIZE,
+    text: '',
+    style: {
+        borderWidth: 0,
+        backgroundColor: BLOCK_COLOR,
+        textColor: BLOCK_COLOR
+    }
+}
+
+let brickWidgets = []
+
+let currentPlayer = {
+    movingLeft: false,
+    movingRight: false,
+    widget:{}
+}
+
+let ball = {
+    angle: 45,
+    speed: 0,
+    widget:{}
+}
